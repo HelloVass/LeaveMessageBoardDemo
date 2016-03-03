585624520c6ae078086bbd7b1d655ae4c1a0b3aa
HelloVass <hellova33@gmail.com>
2016-03-03 22:47:24 +0800

    bug fix

--- app/src/main/java/geeklub/org/messageboarddemo/utils/FaceTextEmotionKeyboardController.java
+++ app/src/main/java/geeklub/org/messageboarddemo/utils/FaceTextEmotionKeyboardController.java
@@ -6,7 +6,10 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
@@ -42,7 +45,6 @@ public class FaceTextEmotionKeyboardController {
     mFaceTextTrigger = builder.mNestedFaceTextTrigger;
     mInputMethodManager = builder.mNestedInputMethodManager;
     mSupportSoftKeyboardUtil = builder.mNestedSupportSoftKeyboardUtil;
-
     setUpCallbacks();
   }
 
@@ -56,25 +58,28 @@ public class FaceTextEmotionKeyboardController {
   }
 
   private void setUpCallbacks() {
-    mFaceTextTrigger.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        if (mFaceTextInputLayout.isShown()) { // 颜文字键盘显示
+    mEditText.requestFocus();
+    mEditText.setOnTouchListener(new View.OnTouchListener() {
+      @Override public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP && mFaceTextInputLayout.isShown()) {
+          Log.i(TAG, "mEditText.onTouch()");
           hideFaceTextInputLayout();
-        } else { // 颜文字键盘隐藏
-          if (mSupportSoftKeyboardUtil.isSoftKeyboardShown()) { // 软键盘显示
-            showFaceTextInputLayout();
-          } else { // 软键盘隐藏
-            showFaceTextInputLayout();
-          }
         }
+        return false;
       }
     });
-    ;
-    mEditText.requestFocus();
-    mEditText.setOnClickListener(new View.OnClickListener() {
+
+    mFaceTextTrigger.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-        if (mFaceTextInputLayout.isShown()) {
+        Log.i(TAG, "mFaceTextTrigger.onClick()");
+        if (mFaceTextInputLayout.isShown()) { // "颜文字键盘"显示
           hideFaceTextInputLayout();
+        } else { // "颜文字键盘"隐藏
+          if (mSupportSoftKeyboardUtil.isSoftKeyboardShown()) { // "软键盘"显示
+            showFaceTextInputLayout();
+          } else { // "软键盘"隐藏
+            dismissFaceTextInputLayout();
+          }
         }
       }
     });
@@ -84,8 +89,7 @@ public class FaceTextEmotionKeyboardController {
    * 显示颜文字键盘
    */
   private void showFaceTextInputLayout() {
-    mFaceTextInputLayout.getLayoutParams().height =
-        mSupportSoftKeyboardUtil.getSupportSoftKeyboardHeight();
+
     mFaceTextInputLayout.setVisibility(View.VISIBLE);
 
     ObjectAnimator showAnimator = ObjectAnimator.ofFloat(mFaceTextInputLayout, "alpha", 0.0F, 1.0F);
@@ -93,7 +97,8 @@ public class FaceTextEmotionKeyboardController {
     showAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
     showAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
       @Override public void onAnimationUpdate(ValueAnimator animation) {
-
+        mFaceTextInputLayout.getLayoutParams().height =
+            mSupportSoftKeyboardUtil.getSupportSoftKeyboardHeight();
       }
     });
     showAnimator.addListener(new AnimatorListenerAdapter() {
@@ -120,18 +125,36 @@ public class FaceTextEmotionKeyboardController {
     hideAnimator.setInterpolator(new AccelerateInterpolator());
     hideAnimator.addListener(new AnimatorListenerAdapter() {
       @Override public void onAnimationStart(Animator animation) {
-        super.onAnimationStart(animation);
         lockContentViewHeight();
         showSoftKeyboard();
       }
 
       @Override public void onAnimationEnd(Animator animation) {
-        super.onAnimationEnd(animation);
         mFaceTextInputLayout.setVisibility(View.GONE);
         unlockContentViewHeight();
       }
     });
     hideAnimator.start();
+  }
+
+  /**
+   * 隐藏颜文字键盘
+   */
+  private void dismissFaceTextInputLayout() {
+    ObjectAnimator dismissAnimator =
+        ObjectAnimator.ofFloat(mFaceTextInputLayout, "alpha", 1.0F, 0.0F);
+    dismissAnimator.setDuration(300);
+    dismissAnimator.setInterpolator(new AccelerateInterpolator());
+    dismissAnimator.addListener(new AnimatorListenerAdapter() {
+      @Override public void onAnimationStart(Animator animation) {
+        showSoftKeyboard();
+      }
+
+      @Override public void onAnimationEnd(Animator animation) {
+        mFaceTextInputLayout.setVisibility(View.GONE);
+      }
+    });
+    dismissAnimator.start();
   }
 
   /**
@@ -215,6 +238,9 @@ public class FaceTextEmotionKeyboardController {
       this.mNestedInputMethodManager =
           (InputMethodManager) mNestedActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
       this.mNestedSupportSoftKeyboardUtil = SupportSoftKeyboardUtil.getInstance(mNestedActivity);
+      mNestedActivity.getWindow()
+          .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+              | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
     }
   }
 }
